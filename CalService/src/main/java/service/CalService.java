8a201d7955fb52cf8f5661398bcd1a3519693b16
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2022-06-13 19:52:02 -0400

    DEMO SOAP SIMPLE WEB SERVICE

---
+++ CalService/src/main/java/service/CalService.java
@@ -0,0 +1,11 @@
+/**
+ * CalService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package service;
+
+public interface CalService extends java.rmi.Remote {
+}
---
+++ CalService/src/main/java/service/CalServiceInf.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface CalServiceInf {
+	public int add(int i , int j);
+}
